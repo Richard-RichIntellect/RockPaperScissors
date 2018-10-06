986b2447d419bfa9dc906a7ed71db9426f8369d7
Richard Samuel <richard@richintellect.com>
2018-10-06 21:07:16 +0100

    Richard-RichIntellect

---
+++ contracts/RockPaperScissors.sol
@@ -0,0 +1,118 @@
+pragma solidity 0.4.24;
+
+contract RockPaperScissors {
+  enum HandGestureEnum {Rock,Paper,Scissors,Nothing}
+
+  struct PlayersStruct
+  {
+    uint playerOneBalance;
+    uint playerTwoBalance;
+    address playerOneAddress;
+    address playerTwoAddress;
+    HandGestureEnum playerOneHandGesture;
+    HandGestureEnum playerTwoHandGesture;
+  }
+
+  mapping (bytes32 => PlayersStruct) public players;
+
+  event LogReturnPlayersKeyAddresses(address playerOneAddr, address playerTwoAddr);
+  event LogAddToPlayerBalance(address player, uint balanceAdded);
+  event LogPlayersMove(address player, HandGestureEnum handGesture);
+  event LogWinner(address player, uint winnings);
+  event LogWithdrawalAmount(address player, uint amount);
+
+  constructor() public {
+
+  }
+
+  function returnPlayersKey(address playerOneAddr, address playerTwoAddr) public view returns(bytes32) {
+    require (playerOneAddr != address(0),"Player one address required");
+    require (playerTwoAddr != address(0),"Player two address required");
+    return keccak256(abi.encodePacked(playerOneAddr,playerTwoAddr, address(this)));
+  }
+
+  function addToPlayerOneBalance(bytes32 playersUniqueKey) public payable
+  {
+    emit LogAddToPlayerBalance(msg.sender, msg.value);
+    require (playersUniqueKey!=0,"playersUniqueKey required");
+    players[playersUniqueKey].playerOneBalance += msg.value;
+    players[playersUniqueKey].playerOneAddress = msg.sender;
+  }
+
+  function addToPlayerTwoBalance(bytes32 playersUniqueKey) public payable
+  {
+    emit LogAddToPlayerBalance(msg.sender, msg.value);
+    require (playersUniqueKey!=0,"playersUniqueKey required");
+    players[playersUniqueKey].playerTwoBalance += msg.value;
+    players[playersUniqueKey].playerTwoAddress = msg.sender;
+  }
+
+  function addPlayerOneMove(bytes32 playersUniqueKey, HandGestureEnum handGesture) public
+  {
+    emit LogPlayersMove(msg.sender, handGesture);
+    require (playersUniqueKey!=0,"playersUniqueKey required");
+    bytes32 localKey = returnPlayersKey(msg.sender, players[playersUniqueKey].playerTwoAddress);
+    require (localKey == playersUniqueKey,"Invalid playersUniqueKey");
+    players[playersUniqueKey].playerOneHandGesture = handGesture;
+  }
+
+  function addPlayerTwoMove(bytes32 playersUniqueKey, HandGestureEnum handGesture) public
+  {
+    emit LogPlayersMove(msg.sender, handGesture);
+    require (playersUniqueKey!=0,"playersUniqueKey required");
+    bytes32 localKey = returnPlayersKey(players[playersUniqueKey].playerOneAddress, msg.sender);
+    require (localKey == playersUniqueKey,"Invalid playersUniqueKey");
+    players[playersUniqueKey].playerTwoHandGesture = handGesture;
+  }
+
+  function play(bytes32 playersUniqueKey) public
+  {
+    require (players[playersUniqueKey].playerOneAddress == msg.sender || players[playersUniqueKey].playerOneAddress == msg.sender,"Unathorized command");
+    require (playersUniqueKey!=0,"playersUniqueKey required");
+    require (players[playersUniqueKey].playerOneHandGesture != HandGestureEnum.Nothing,"Player One has not decided gesture");
+    require (players[playersUniqueKey].playerTwoHandGesture != HandGestureEnum.Nothing,"Player Two has not decided gesture");
+    
+
+    uint winnings = players[playersUniqueKey].playerOneBalance + players[playersUniqueKey].playerTwoBalance;
+    players[playersUniqueKey].playerOneHandGesture = HandGestureEnum.Nothing;
+    players[playersUniqueKey].playerTwoHandGesture = HandGestureEnum.Nothing;      
+    
+    if (uint(players[playersUniqueKey].playerOneHandGesture)-uint(players[playersUniqueKey].playerTwoHandGesture)+5 % 3 == 0)
+    {
+      emit LogWinner(players[playersUniqueKey].playerOneAddress,winnings);
+      players[playersUniqueKey].playerOneBalance = winnings;
+      players[playersUniqueKey].playerTwoBalance = 0;
+    }
+    else
+    {
+      emit LogWinner(players[playersUniqueKey].playerTwoAddress,winnings);
+      players[playersUniqueKey].playerOneBalance = 0;
+      players[playersUniqueKey].playerTwoBalance = winnings;
+    }
+
+  }
+
+  function withdrawWinnings(bytes32 playersUniqueKey, uint withdrawalAmount) public
+  {
+
+    require (playersUniqueKey!=0,"playersUniqueKey required");
+    require (withdrawalAmount > 0,"withdrawalAmount must be greater than zero");
+    require (players[playersUniqueKey].playerOneAddress == msg.sender || players[playersUniqueKey].playerTwoAddress == msg.sender,"Unathorized withdrawal");
+    
+    if (players[playersUniqueKey].playerOneAddress == msg.sender) {
+      emit LogWithdrawalAmount(players[playersUniqueKey].playerOneAddress, withdrawalAmount);
+      require(players[playersUniqueKey].playerOneBalance >= withdrawalAmount, "Insufficient funds.");
+      players[playersUniqueKey].playerOneBalance -= withdrawalAmount;
+      players[playersUniqueKey].playerOneAddress.transfer(withdrawalAmount);
+    }
+
+    if (players[playersUniqueKey].playerTwoAddress == msg.sender) {
+      emit LogWinner(players[playersUniqueKey].playerTwoAddress,withdrawalAmount);
+      require(players[playersUniqueKey].playerTwoBalance >= withdrawalAmount, "Insufficient funds.");
+      players[playersUniqueKey].playerTwoBalance -= withdrawalAmount;
+      players[playersUniqueKey].playerTwoAddress.transfer(withdrawalAmount);
+    }
+
+  }
+
+}
---
+++ test/RockPaperScissorsTest.js
@@ -0,0 +1,217 @@
+var RockPaperScissors = artifacts.require("../contracts/RockPaperScissors.sol");
+contract('RockPaperScissors', function (accounts) {
+  let instance;
+  let playerHash;
+  let playerOneBalance;
+  let playerTwoBalance;
+
+
+  const playerOne = accounts[0];
+  const playerTwo = accounts[1];
+  const owner = accounts[2];
+
+  const emptyAddress = /^0x0+$/
+
+  beforeEach(function () {
+    return RockPaperScissors.new({ from: owner })
+      .then(function (_instance) {
+        instance = _instance;
+        console.log(!instance);
+        playerOneBalance = web3.eth.getBalance(playerOne);
+        playerTwoBalance = web3.eth.getBalance(playerTwo);
+        return instance.returnPlayersKey(playerOne, playerTwo)
+        .then(result => {
+          console.log(!instance);
+          playerHash = result;
+          console.log(!instance);
+        })
+        .catch(result => console.log("Error:",result));  
+      })
+      .catch(result => console.log("Error:",result));
+  });
+
+
+  console.log(!instance);
+
+  it("should not be able to get password hash if password hashes is empty", function () {
+    console.log(!instance);
+    return instance.returnPlayersKey(0, 0,  { from: owner })
+      .then(result => {
+        assert.isTrue(false);
+      })
+      .catch(result => {
+        console.log(result);
+        assert.isTrue(true);
+      })
+  });
+
+  it("should not be able to add to player one balance if password hash is empty", function () {
+    return instance.addToPlayerOneBalance(0,   { from: playerOne, value: web3.toWei('0.1', 'ether') })
+      .then(result => {
+        assert.isTrue(false);
+      })
+      .catch(result => {
+        console.log(result);
+        assert.isTrue(true);
+      })
+  });
+
+  it("should not be able to add to player two balance if password hash is empty", function () {
+    return instance.addToPlayerTwoBalance(0,   { from: playerTwo, value: web3.toWei('0.1', 'ether') })
+      .then(result => {
+        assert.isTrue(false);
+      })
+      .catch(result => {
+        console.log(result);
+        assert.isTrue(true);
+      })
+  });
+
+  it("should not be able to add Player One move if password hash is empty", function () {
+    return instance.addPlayerOneMove(0, 0,  { from: owner })
+      .then(result => {
+        assert.isTrue(false);
+      })
+      .catch(result => {
+          console.log(result);
+        assert.isTrue(true);
+      })
+  });
+
+
+
+  it("should not be able to add Player Two move if password hash is empty", function () {
+    return instance.addPlayerTwoMove(0, 0,  { from: owner })
+      .then(result => {
+        assert.isTrue(false);
+      })
+      .catch(result => {
+          console.log(result);
+        assert.isTrue(true);
+      })
+  });
+
+  it("should not be able to add Player One move if it is not a player", function () {
+    return instance.addToPlayerOneBalance(playerHash, {from:playerOne})
+    .then (result => {
+        return instance.addPlayerOneMove(playerHash, 0,  { from: owner })
+          .then(result => {
+            assert.isTrue(false);
+          })
+          .catch(result => {
+              console.log(result);
+            assert.isTrue(true);
+          })
+        });
+  });
+
+  
+
+  it("should not be able to add Player Two move if it is not a player", function () {
+    return instance.addToPlayerTwoBalance(playerHash, {from:playerOne})
+    .then (result => {
+        return instance.addPlayerTwoMove(playerHash, 0,  { from: owner })
+          .then(result => {
+            assert.isTrue(false);
+          })
+          .catch(result => {
+              console.log(result);
+            assert.isTrue(true);
+          })
+        });
+  });
+
+
+  it("should not be able to add Player Two move if it is not a player", function () {
+    return instance.addToPlayerTwoBalance(playerHash, {from:playerOne, value:web3.toWei('0.1', 'ether')})
+    .then (result => {
+        return instance.addPlayerTwoMove(playerHash, 0,  { from: owner })
+          .then(result => {
+            assert.isTrue(false);
+          })
+          .catch(result => {
+              console.log(result);
+            assert.isTrue(true);
+          })
+        });
+  });
+
+  it("should not be able to play if a player doesn't activate it.",async function () {
+
+    try {
+      await instance.addToPlayerOneBalance(playerHash, {from:playerOne, value:web3.toWei('0.1', 'ether')});
+      await instance.addToPlayerTwoBalance(playerHash, {from:playerTwo, value:web3.toWei('0.1', 'ether')});
+      await instance.addPlayerOneMove(playerHash,0, {from:playerOne});
+      await instance.addPlayerTwoMove(playerHash,0, {from:playerTwo});
+
+    } catch (error) {
+      console.log(error);
+      assert.isTrue(false);
+    }
+
+
+    return instance.play(playerHash,  { from: owner })
+      .then(result => {
+        console.log(result);
+        assert.isTrue(false);
+      })
+      .catch(result => {
+
+        assert.isTrue(true);
+      })
+  });
+
+
+  it("should not be able to play if a player doesn't have valid hand gestures",async function () {
+
+    try {
+      await instance.addToPlayerOneBalance(playerHash, {from:playerOne, value:web3.toWei('0.1', 'ether')});
+      await instance.addToPlayerTwoBalance(playerHash, {from:playerTwo, value:web3.toWei('0.1', 'ether')});
+      await instance.addPlayerOneMove(playerHash,3, {from:playerOne});
+      await instance.addPlayerTwoMove(playerHash,3, {from:playerTwo});
+
+    } catch (error) {
+      console.log(error);
+      assert.isTrue(true);
+    }
+
+
+    return instance.play(playerHash,  { from: owner })
+      .then(result => {
+        console.log(result);
+        assert.isTrue(false);
+      })
+      .catch(result => {
+
+        assert.isTrue(true);
+      })
+  });
+
+  
+  it("should  be able to play", async function () {
+
+    try {
+      await instance.addToPlayerOneBalance(playerHash, {from:playerOne, value:web3.toWei('0.1', 'ether')});
+      await instance.addToPlayerTwoBalance(playerHash, {from:playerTwo, value:web3.toWei('0.1', 'ether')});
+      await instance.addPlayerOneMove(playerHash,0, {from:playerOne});
+      await instance.addPlayerTwoMove(playerHash,1, {from:playerTwo});
+
+    } catch (error) {
+      console.log(error);
+      assert.isTrue(false);
+    }
+
+    return instance.play(playerHash,  { from: playerOne })
+      .then(result => {
+        return instance.withdrawWinnings(playerHash,web3.toWei('0.1', 'ether'),  { from: playerTwo })
+        .then (result => {
+        assert.isTrue(true);
+        }
+      )
+      .catch(result => {
+        console.log(result);
+        assert.isTrue(false);
+      })});
+  });
+
+});
