a7c92135e5ee2eb10d4a1dc40d0891646b94a389
Richard Samuel <richard@richintellect.com>
2018-10-08 23:14:01 +0100

    updated contract  and test.

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -15,7 +15,8 @@ contract RockPaperScissors {
   uint timeLimit = 1 days;
 
   mapping (bytes32 => PlayersStruct) public players;
-
+  mapping (bytes32 => bool) public gameKeyUsed;
+  mapping (address => uint) public playersBalances;
 
   event LogAddToPlayerBalance(address player, uint balanceAdded);
   event LogWinner(address player, uint winnings);
@@ -26,25 +27,25 @@ contract RockPaperScissors {
 
   }
 
-  function returnPlayersKey() public view returns(bytes32) {
+  function generateNewGameKey() public view returns(bytes32) {
 	  return keccak256(abi.encodePacked(msg.sender,now, address(this)));
   }
 
-  function playerOneStartGame(bytes32 playersUniqueKey,HandGestureEnum handGesture) public payable  {
-    playerOneStartGameWithFunds(playersUniqueKey,handGesture,0);
-  }
 
-  function playerOneStartGameWithFunds(bytes32 playersUniqueKey,HandGestureEnum handGesture, bytes32 oldWinnings) public payable {
+
+  function playerOneStartGameWithFunds(bytes32 playersUniqueKey,HandGestureEnum handGesture, bytes32 oldWinnings) public payable 
+  {
 	  emit LogAddToPlayerBalance(msg.sender, msg.value);
     uint additionalFunds = 0;
     if (oldWinnings!=0)
     {
       require (players[playersUniqueKey].winnersAddress == msg.sender,"Unathorized request.");
       require (players[playersUniqueKey].winnings != 0,"No funds to transfer");
+      require (!gameKeyUsed[playersUniqueKey],"Game key has been used.");
+      gameKeyUsed[playersUniqueKey] = true;
       additionalFunds = players[playersUniqueKey].winnings;
       players[playersUniqueKey].winnings = 0;
       emit LogWithdrawalAmount(msg.sender,additionalFunds);
-
     }
     players[playersUniqueKey].winnings = msg.value + additionalFunds;
     players[playersUniqueKey].winnersAddress = msg.sender;
@@ -52,18 +53,13 @@ contract RockPaperScissors {
     players[playersUniqueKey].playerOneEntryDateTimeStamp = now;
   }
 
-  function playerTwoEnterAndPlay(bytes32 playersUniqueKey, HandGestureEnum handGesture) public payable
-  {
-    playerTwoEnterAndPlayWithFunds(playersUniqueKey,handGesture,0);
-  }
-
+ 
   function playerTwoEnterAndPlayWithFunds(bytes32 playersUniqueKey, HandGestureEnum handGesture, bytes32 oldWinnings) public payable
   {
     emit LogAddToPlayerBalance(msg.sender, msg.value);
     require (players[playersUniqueKey].playerOneEntryDateTimeStamp!=0,"Cannot find matching game key");
     require (playersUniqueKey!=0,"playersUniqueKey required");
     require (!players[playersUniqueKey].complete,"Game completed.");
-    require (players[playersUniqueKey].winnings==msg.value,"You must put in the same amount to play");
     players[playersUniqueKey].complete = true;
     uint additionalFunds = 0;
 
@@ -72,23 +68,25 @@ contract RockPaperScissors {
 
       require (players[playersUniqueKey].winnersAddress == msg.sender,"Unathorized request.");
       require (players[playersUniqueKey].winnings != 0,"No funds to transfer");
-      additionalFunds = players[playersUniqueKey].winnings;
-      players[playersUniqueKey].winnings = 0;
+      additionalFunds = players[oldWinnings].winnings;
+      players[oldWinnings].winnings = 0;
       emit LogTransferAmount(msg.sender,additionalFunds);
 
     }
 
-    players[playersUniqueKey].winnings += (msg.value + additionalFunds);
+    uint totalWinningsAmount = players[playersUniqueKey].winnings + msg.value + additionalFunds;
    
     if (uint(players[playersUniqueKey].playerOneHandGesture)-uint(handGesture)+5 % 3 == 0)
     {
-      emit LogWinner(players[playersUniqueKey].winnersAddress,players[playersUniqueKey].winnings);
+      emit LogWinner(players[playersUniqueKey].winnersAddress,totalWinningsAmount);
+      playersBalances[players[playersUniqueKey].winnersAddress] += totalWinningsAmount;
     }
     else
     {
       if (uint(players[playersUniqueKey].playerOneHandGesture)-uint(handGesture)+5 % 3 == 1)
       {
         emit LogWinner(msg.sender,players[playersUniqueKey].winnings);
+        playersBalances[msg.sender] += totalWinningsAmount;
         players[playersUniqueKey].winnersAddress = msg.sender;
       }
       else
@@ -103,12 +101,12 @@ contract RockPaperScissors {
   {
     require (playersUniqueKey!=0,"playersUniqueKey required");
     require (players[playersUniqueKey].winnersAddress == msg.sender,"Invalid address");
-    require (players[playersUniqueKey].winnings > 0,"No funds available");
+    require (playersBalances[msg.sender] > 0,"No funds available");
     require (players[playersUniqueKey].complete || (!players[playersUniqueKey].complete && players[playersUniqueKey].playerOneEntryDateTimeStamp + timeLimit <= now),"Cannot withdraw funds.");
     emit LogWithdrawalAmount(msg.sender,players[playersUniqueKey].winnings);
-    uint winnings = players[playersUniqueKey].winnings;
-    players[playersUniqueKey].winnings = 0;
-    players[playersUniqueKey].winnersAddress.transfer(winnings);
+    uint playerBalance = playersBalances[msg.sender];
+    playersBalances[msg.sender] = 0;
+    players[playersUniqueKey].winnersAddress.transfer(playerBalance);
   }
 
 }
--- test/RockPaperScissorsTest.js
+++ test/RockPaperScissorsTest.js
@@ -1,4 +1,6 @@
-var RockPaperScissors = artifacts.require("../contracts/RockPaperScissors.sol");
+const RockPaperScissors = artifacts.require("../contracts/RockPaperScissors.sol");
+const expectedExceptionPromise = require("./expected_exception_testRPC_and_geth.js");
+
 contract('RockPaperScissors', function (accounts) {
   let instance;
   let playersHash;
@@ -14,29 +16,33 @@ contract('RockPaperScissors', function (accounts) {
 
   beforeEach(function () {
     return RockPaperScissors.new({ from: owner })
-      .then(function (_instance) {
+      .then(async function (_instance) {
         instance = _instance;
-      
-        playerOneBalance = web3.eth.getBalance(playerOne);
-        playerTwoBalance = web3.eth.getBalance(playerTwo);
-        return instance.returnPlayersKey()
-        .then (result => {
-          playersHash = result;
-          console.log('<<>>',result);
-          return instance.playerOneStartGame(playersHash,0,{ from: playerOne, value: web3.toWei('0.1', 'ether') })
-        })
-        .catch(result => console.log("Error:",result));  
+
+        playerOneBalance = await web3.eth.getBalance(playerOne);
+        playerTwoBalance = await web3.eth.getBalance(playerTwo);
+        return instance.generateNewGameKey();
       })
-      .catch(result => console.log("Error:",result));
+      .then(result => {
+        playersHash = result;
+      })
+      .catch(result => console.log("Error:", result));
   });
 
 
-  console.log(!instance);
 
+console.log(!instance);
+
+describe("A suite of functions to test RockPaperScissors", function () { 
+
+  beforeEach(function () {
+    return instance.playerOneStartGameWithFunds(playersHash, 0, 0, { from: playerOne, value: web3.toWei('0.1', 'ether') })
+    .catch(results => console.log(results)); 
+  });
 
   it("should not be able to to let player two play if password hash is empty", function () {
     console.log(!instance);
-    return instance.playerTwoEnterAndPlay(0, 0,  { from: owner })
+    return instance.playerOneStartGameWithFunds(playersHash, 0, 0, { from: owner })
       .then(result => {
         assert.isTrue(false);
       })
@@ -45,14 +51,12 @@ contract('RockPaperScissors', function (accounts) {
         assert.isTrue(true);
       })
   });
-
-
-
-  it("should not be able to allow player two to play if game already completed.",async function () {
-    
-    await instance.playerTwoEnterAndPlay(playersHash,1, { from: playerTwo, value: web3.toWei('0.1', 'ether') })
-    
-    return instance.playerTwoEnterAndPlay(playersHash,1, { from: playerTwo, value: web3.toWei('0.1', 'ether') })
+  
+  it("should not be able to allow player two to play if game already completed.", async function () {
+  
+    await instance.playerOneStartGameWithFunds(playersHash, 1,0, { from: playerTwo, value: web3.toWei('0.1', 'ether') })
+  
+    return instance.playerOneStartGameWithFunds(playersHash, 1,0, { from: playerTwo, value: web3.toWei('0.1', 'ether') })
       .then(result => {
         console.log(result);
         assert.isTrue(false);
@@ -62,11 +66,10 @@ contract('RockPaperScissors', function (accounts) {
         assert.isTrue(true);
       })
   });
-
-
-  it("should not be able to allow player two to play betting amount is not equal.",function () {
-    
-    return instance.playerTwoEnterAndPlay(playersHash,0, { from: playerTwo, value: web3.toWei('0.01', 'ether') })
+  
+  it("should not be able to allow player two to play betting amount is not equal.", function () {
+  
+    return instance.playerOneStartGameWithFunds(playersHash, 0,0, { from: playerTwo, value: web3.toWei('0.01', 'ether') })
       .then(result => {
         assert.isTrue(false);
       })
@@ -75,15 +78,11 @@ contract('RockPaperScissors', function (accounts) {
         assert.isTrue(true);
       })
   });
-
-
-
-
-
-  it("should not be able to withdraw balance if hash is empty.",async function () {
-    
-    await instance.playerTwoEnterAndPlay(playersHash,1, { from: playerTwo, value: web3.toWei('0.01', 'ether') })
-
+  
+  it("should not be able to withdraw balance if hash is empty.", async function () {
+  
+    await instance.playerOneStartGameWithFunds(playersHash, 1,0, { from: playerTwo, value: web3.toWei('0.01', 'ether') })
+  
     return instance.withdrawBalance(0, { from: playerTwo })
       .then(result => {
         assert.isTrue(false);
@@ -93,109 +92,120 @@ contract('RockPaperScissors', function (accounts) {
         assert.isTrue(true);
       })
   });
+});
 
-  it("should not be able to withdrawer balance unless you are the winner.",async function () {
-    
-    let newHash = await instance.returnPlayersKey();
+let newHash;
 
-    await instance.playerOneStartGame(newHash,0,{ from: playerOne, value: web3.toWei('0.1', 'ether') })
- 
-    await instance.playerTwoEnterAndPlay(newHash,1, { from: playerTwo, value: web3.toWei('0.1', 'ether') })
+describe ("A suite of functions to test RockPaperScissors with new instance", () =>{ 
 
-    return instance.withdrawBalance(newHash, { from: owner })
-      .then(result => {
-        assert.isTrue(false);
-      })
-      .catch(result => {
-        console.log(result);
-        assert.isTrue(true);
-      })
+    beforeEach(function () {
+      return instance.generateNewGameKey()
+      .then(result => newHash = result)
+      .catch(results => console.log(results)); 
+    });
+
+    beforeEach(function () {
+      return instance.playerOneStartGameWithFunds(newHash, 0, 0, { from: playerOne, value: web3.toWei('0.1', 'ether') })
+      .catch(results => console.log(results)); 
+    });
+
+    it("should not be able to withdraw balance unless you are the winner.", async function () {
+
+      await instance.playerTwoEnterAndPlayWithFunds(newHash, 1, 0, { from: playerTwo, value: web3.toWei('0.1', 'ether') })
+
+      return expectedExceptionPromise(function () {return instance.withdrawBalance(newHash, { from: owner });
+        // .then(result => {
+        //   assert.isTrue(false);
+        // })
+        // .catch(result => {
+        //   console.log(result);
+        //   assert.isTrue(true);
+        // })
+    });
   });
 
 
-  it("should not be able to transfer balance unless you are the winner in the prior game.",async function () {
-    
-    await instance.playerTwoEnterAndPlay(playersHash,1, { from: playerTwo, value: web3.toWei('0.1', 'ether') })
+    it("should not be able to transfer balance unless you are the winner in the prior game.", async function () {
 
-    let newHash = await instance.returnPlayersKey();
+      await instance.playerTwoEnterAndPlayWithFunds(newHash, 1, 0, { from: playerTwo, value: web3.toWei('0.1', 'ether') })
 
-    await instance.playerOneStartGame(newHash,0,{ from: owner, value: web3.toWei('0.1', 'ether') });
+      let anotherNewHash = await instance.generateNewGameKey();
 
-    return instance.playerTwoEnterAndPlayWithFunds(newHash,1,0, { from: playerTwo })
-      .then(result => {
-        assert.isTrue(false);
-      })
-      .catch(result => {
-        console.log(result);
-        assert.isTrue(true);
-      })
+      await instance.playerOneStartGameWithFunds(anotherNewHash, 0,0, { from: owner, value: web3.toWei('0.1', 'ether') });
+
+      return instance.playerTwoEnterAndPlayWithFunds(anotherNewHash, 1, newHash, { from: playerTwo })
+        .then(result => {
+          assert.isTrue(false);
+        })
+        .catch(result => {
+          console.log(result);
+          assert.isTrue(true);
+        })
+    });
+
+
+
+    it("should not be able to transfer balance unless you have funds.", async function () {
+
+      await instance.playerTwoEnterAndPlayWithFunds(newHash, 1, 0, { from: playerTwo, value: web3.toWei('0.1', 'ether') });
+
+      await instance.withdrawBalance(newHash, { from: playerTwo });
+
+
+      let anotherNewHash = await instance.generateNewGameKey();
+
+      await instance.playerOneStartGameWithFunds(anotherNewHash, 0, 0, { from: playerOne, value: web3.toWei('0.1', 'ether') });
+
+      return instance.playerTwoEnterAndPlayWithFunds(anotherNewHash, 1,0, newHash, { from: playerTwo, value: web3.toWei('0.0', 'ether') })
+        .then(result => {
+          assert.isTrue(false);
+        })
+        .catch(result => {
+          console.log(result);
+          assert.isTrue(true);
+        })
+    });
+
+
+
+    it("should allow Player one to win.", async function () {
+
+      await instance.playerTwoEnterAndPlayWithFunds(newHash, 1, 0, { from: playerTwo, value: web3.toWei('0.01', 'ether') })
+
+      return instance.withdrawBalance(newHash, { from: playerTwo })
+        .then(result => {
+          assert.isTrue(true);
+        })
+        .catch(result => {
+          console.log(result);
+          assert.isTrue(true);
+        })
+    });
+
+    it("should allow Player two to win.", async function () {
+
+      await instance.playerTwoEnterAndPlayWithFunds(newHash, 1, 0, { from: playerTwo, value: web3.toWei('0.01', 'ether') })
+
+      return instance.withdrawBalance(newHash, { from: playerTwo })
+        .then(result => {
+          assert.isTrue(true);
+        })
+        .catch(result => {
+          console.log(result);
+          assert.isTrue(true);
+        })
+    });
+
+    it("should allow a draw.", async function () {
+
+      return instance.playerTwoEnterAndPlayWithFunds(newHash, 0, 0,{ from: playerTwo, value: web3.toWei('0.01', 'ether') })
+        .then(result => {
+          assert.isTrue(false);
+        })
+        .catch(result => {
+          console.log(result);
+          assert.isTrue(true);
+        })
+    });
   });
-
-
-
-  it("should not be able to transfer balance unless you have funds.",async function () {
-    
-    await instance.playerTwoEnterAndPlay(playersHash,1, { from: playerTwo, value: web3.toWei('0.1', 'ether') });
-
-    await instance.withdrawBalance(playersHash, { from: playerTwo });
-
-
-    let newHash = await instance.returnPlayersKey();
-    
-    await instance.playerOneStartGame(newHash, 0,{ from: playerOne, value: web3.toWei('0.1', 'ether') });
-
-    return instance.playerTwoEnterAndPlayWithFunds(newHash,1,playersHash, { from: playerTwo })
-      .then(result => {
-        assert.isTrue(false);
-      })
-      .catch(result => {
-        console.log(result);
-        assert.isTrue(true);
-      })
-  });
-
-
-  it("should allow Player one to win.",async function () {
-    
-    await instance.playerTwoEnterAndPlay(playersHash,1, { from: playerTwo, value: web3.toWei('0.01', 'ether') })
-
-    return instance.withdrawBalance(playersHash, { from: playerTwo })
-      .then(result => {
-        assert.isTrue(true);
-      })
-      .catch(result => {
-        console.log(result);
-        assert.isTrue(true);
-      })
-  });
-
-  it("should allow Player two to win.",async function () {
-    
-    await instance.playerTwoEnterAndPlay(playersHash,1, { from: playerTwo, value: web3.toWei('0.01', 'ether') })
-
-    return instance.withdrawBalance(playersHash, { from: playerTwo })
-      .then(result => {
-        assert.isTrue(true);
-      })
-      .catch(result => {
-        console.log(result);
-        assert.isTrue(true);
-      })
-  });
-
-  it("should allow a draw.",async function () {
-    
-    return instance.playerTwoEnterAndPlay(playersHash,0, { from: playerTwo, value: web3.toWei('0.01', 'ether') })
-      .then(result => {
-        assert.isTrue(false);
-      })
-      .catch(result => {
-        console.log(result);
-        assert.isTrue(true);
-      })
-  });
-
-
-
-
 });
